704b3288982fa7c29def401b84ba62a7969e83c6
YurijM <y-myagkov@mail.ru>
2024-01-04 16:58:55 +0300

    Удалён commonRepository

--- app/src/main/java/com/mu/tote2024/di/module/repository/GamblerRepositoryModule.kt
+++ app/src/main/java/com/mu/tote2024/di/module/repository/GamblerRepositoryModule.kt
@@ -3,7 +3,6 @@ package com.mu.tote2024.di.module.repository
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.storage.FirebaseStorage
 import com.mu.tote2024.data.repository.GamblerRepositoryImpl
-import com.mu.tote2024.domain.repository.CommonRepository
 import com.mu.tote2024.domain.repository.GamblerRepository
 import com.mu.tote2024.domain.usecase.gambler_usecase.GamblerUseCase
 import com.mu.tote2024.domain.usecase.gambler_usecase.GetGambler
@@ -21,10 +20,9 @@ object GamblerRepositoryModule {
     @Provides
     @Singleton
     fun provideGamblerRepository(
-        commonRepository: CommonRepository,
         database: FirebaseDatabase,
         storage: FirebaseStorage
-    ) : GamblerRepository = GamblerRepositoryImpl(commonRepository, database, storage)
+    ) : GamblerRepository = GamblerRepositoryImpl(database, storage)
 
     @Provides
     @Singleton
