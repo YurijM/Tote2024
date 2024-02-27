faa2d7f11164c6d773bf41ce6a059173fe5024b6
YurijM <y-myagkov@mail.ru>
2024-02-27 11:18:58 +0300

    В функцию getTableResult теперь передается список игр только группового этапа.

--- app/src/main/java/com/mu/tote2024/presentation/screen/game/list/GameListViewModel.kt
+++ app/src/main/java/com/mu/tote2024/presentation/screen/game/list/GameListViewModel.kt
@@ -10,6 +10,8 @@ import com.mu.tote2024.domain.usecase.game_usecase.GameUseCase
 import com.mu.tote2024.domain.usecase.team_usecase.TeamUseCase
 import com.mu.tote2024.presentation.ui.common.UiState
 import com.mu.tote2024.presentation.utils.Constants.EMPTY
+import com.mu.tote2024.presentation.utils.Constants.GROUPS
+import com.mu.tote2024.presentation.utils.Constants.GROUPS_COUNT
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -66,7 +68,10 @@ class GameListViewModel @Inject constructor(
                                     )
                                 )
                             }
-                            val resultTeams = getTableResult(teams, games)
+                            val resultTeams = getTableResult(
+                                teams,
+                                games.filter { game -> game.group in GROUPS.take(GROUPS_COUNT) }
+                            )
 
                             _state.value = GameListState(UiState.Success(resultTeams))
                         }
@@ -75,113 +80,6 @@ class GameListViewModel @Inject constructor(
             }
         }
     }
-
-    /*private fun getTableResult() {
-        viewModelScope.launch {
-            teamUseCase.getTeamList().collect { stateTeams ->
-                _state.value = GameListState(UiState.Loading)
-
-                if (stateTeams is UiState.Success) {
-                    val listTeam = stateTeams.data
-                        .sortedWith(
-                            compareBy<TeamModel> { it.group }
-                                .thenBy { it.itemNo }
-                        )
-
-                    gameUseCase.getGameList().collect { stateGame ->
-                        if (stateGame is UiState.Success) {
-                            val resultTeams = mutableListOf<GroupTeamResultModel>()
-
-                            val listGame = stateGame.data
-
-                            listTeam.forEach { team ->
-                                var resultTeam = GroupTeamResultModel().copy(
-                                    group = team.group,
-                                    team = team.team,
-                                    teamNo = team.itemNo
-                                )
-
-                                var win = 0
-                                var draw = 0
-                                var defeat = 0
-                                var balls1 = 0
-                                var balls2 = 0
-                                var points = 0
-
-                                listGame.filter { it.team1 == team.team || it.team2 == team.team }
-                                    .forEach { game ->
-                                        val goal1: String
-                                        val goal2: String
-                                        var score = ""
-                                        val team2ItemNo: Int
-                                        val teamNo = if (game.team1 == team.team) 1 else 2
-
-                                        if (teamNo == 1) {
-                                            goal1 = game.goal1
-                                            goal2 = game.goal2
-                                            team2ItemNo = listTeam.first { it.team == game.team2 }.itemNo
-                                        } else {
-                                            goal1 = game.goal2
-                                            goal2 = game.goal1
-                                            team2ItemNo = listTeam.first { it.team == game.team1 }.itemNo
-                                        }
-
-                                        if (goal1.isNotBlank() && goal2.isNotBlank()) {
-                                            score = "$goal1:$goal2"
-                                            balls1 += goal1.toInt()
-                                            balls2 += goal2.toInt()
-
-                                            when {
-                                                goal1 > goal2 -> {
-                                                    win++
-                                                    points += 3
-                                                }
-
-                                                goal1 == goal2 -> {
-                                                    draw++
-                                                    points++
-                                                }
-
-                                                else -> defeat++
-                                            }
-                                        }
-
-                                        resultTeam = when (team2ItemNo) {
-                                            1 -> resultTeam.copy(score1 = score)
-                                            2 -> resultTeam.copy(score2 = score)
-                                            3 -> resultTeam.copy(score3 = score)
-                                            4 -> resultTeam.copy(score4 = score)
-                                            else -> resultTeam
-                                        }
-
-                                        resultTeam = when (team.itemNo) {
-                                            1 -> resultTeam.copy(score1 = EMPTY)
-                                            2 -> resultTeam.copy(score2 = EMPTY)
-                                            3 -> resultTeam.copy(score3 = EMPTY)
-                                            4 -> resultTeam.copy(score4 = EMPTY)
-                                            else -> resultTeam
-                                        }
-                                    }
-
-                                resultTeam = resultTeam.copy(
-                                    win = win,
-                                    draw = draw,
-                                    defeat = defeat,
-                                    balls1 = balls1,
-                                    balls2 = balls2,
-                                    points = points,
-                                )
-
-                                resultTeams.add(resultTeam)
-                            }
-
-                            _state.value = GameListState(UiState.Success(resultTeams))
-                        }
-                    }
-                }
-            }
-        }
-    }*/
 
     private fun getTableResult(listTeam: List<TeamModel>, games: List<GameModel>): List<GroupTeamResultModel> {
         val resultTeams = mutableListOf<GroupTeamResultModel>()
@@ -271,7 +169,6 @@ class GameListViewModel @Inject constructor(
 
     companion object {
         data class GameListState(
-            //val result: UiState<List<GameModel>> = UiState.Default
             val result: UiState<List<GroupTeamResultModel>> = UiState.Default
         )
     }
